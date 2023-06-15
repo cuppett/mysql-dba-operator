647d386c19a3d800d0ce87a8ba71532902b8ef0f
Stephen Cuppett <steve@brightframe.com>
2023-06-14 21:47:21 -0400

    Add: Tests for AdminConnection_Types.DatabaseMine

--- api/v1alpha1/adminconnection_types_test.go
+++ api/v1alpha1/adminconnection_types_test.go
@@ -1,13 +1,18 @@
 package v1alpha1
 
 import (
+	"github.com/cuppett/mysql-dba-operator/orm"
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
-var _ = Describe("Admin Connection", func() {
+var _ = Describe("AdminConnection_Types", func() {
 
-	Describe("Testing AdminConnection for database and user happy paths", func() {
+	Describe("AllowedNamespace", func() {
 		DescribeTable("Namespace rules",
 			func(namespaceList []string, name string, good bool) {
 				adminConnection.Spec.AllowedNamespaces = namespaceList
@@ -31,4 +36,131 @@ var _ = Describe("Admin Connection", func() {
 		)
 	})
 
+	Describe("DatabaseMine", func() {
+		var gormDB *gorm.DB
+		var err error
+		var managedDatabase *orm.ManagedDatabase
+		var database *Database
+		cache := make(map[types.UID]*orm.ConnectionDefinition)
+		var saveGormDb, createDb bool
+
+		BeforeEach(func() {
+			gormDB, err = adminConnection.GetDatabaseConnection(ctx, k8sClient, cache)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(gormDB).NotTo(BeNil())
+
+			// Wipe the database table here.
+			gormDB.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&orm.ManagedDatabase{})
+
+			newUID := uuid.New()
+			database = &Database{
+				ObjectMeta: metav1.ObjectMeta{
+					UID:       types.UID(newUID.String()),
+					Namespace: "default",
+					Name:      "test",
+				},
+				Spec: DatabaseSpec{
+					Name: "test",
+				},
+			}
+
+			managedDatabase = &orm.ManagedDatabase{
+				Uuid:         newUID.String(),
+				Namespace:    database.Namespace,
+				Name:         database.Name,
+				DatabaseName: database.Spec.Name,
+			}
+			saveGormDb = true
+			createDb = true
+		})
+
+		JustBeforeEach(func() {
+			if saveGormDb {
+				tx := gormDB.Create(managedDatabase)
+				Expect(tx.Error).To(BeNil())
+			}
+
+			if createDb {
+				createQuery := "CREATE DATABASE `" + database.Spec.Name + "`"
+				tx := gormDB.Exec(createQuery)
+				Expect(tx.Error).To(BeNil())
+			}
+		})
+
+		JustAfterEach(func() {
+			if createDb {
+				dropQuery := "DROP DATABASE IF EXISTS `" + database.Spec.Name + "`"
+				tx := gormDB.Exec(dropQuery)
+				Expect(tx.Error).To(BeNil())
+			}
+		})
+
+		Context("Database does exist in the table, and in the server, and it is a match.", func() {
+			It("returns true for mine.", func() {
+				isDatabaseMine := adminConnection.DatabaseMine(gormDB, database)
+				Expect(isDatabaseMine).To(BeTrue())
+			})
+		})
+
+		Context("Database doesn't exist in the table or server", func() {
+			BeforeEach(func() {
+				createDb = false
+				saveGormDb = false
+			})
+			It("returns true for mine.", func() {
+				isDatabaseMine := adminConnection.DatabaseMine(gormDB, database)
+				Expect(isDatabaseMine).To(BeTrue())
+			})
+		})
+
+		Context("Database does exist in the table, but not in the server", func() {
+			BeforeEach(func() {
+				createDb = false
+			})
+			It("returns true for mine.", func() {
+				isDatabaseMine := adminConnection.DatabaseMine(gormDB, database)
+				Expect(isDatabaseMine).To(BeTrue())
+			})
+		})
+
+		Context("Database does exist in the table, and in the server, but not my UID", func() {
+			BeforeEach(func() {
+				managedDatabase.Uuid = uuid.New().String()
+			})
+			It("returns false for mine.", func() {
+				isDatabaseMine := adminConnection.DatabaseMine(gormDB, database)
+				Expect(isDatabaseMine).To(BeFalse())
+			})
+		})
+
+		Context("Database does exist in the table, and in the server, but not my k8s name", func() {
+			BeforeEach(func() {
+				managedDatabase.Name = "wrongk8sname"
+			})
+			It("returns false for mine.", func() {
+				isDatabaseMine := adminConnection.DatabaseMine(gormDB, database)
+				Expect(isDatabaseMine).To(BeFalse())
+			})
+		})
+
+		Context("Database does exist in the table, and in the server, but not my k8s namespace", func() {
+			BeforeEach(func() {
+				managedDatabase.Namespace = "wrongnamespace"
+			})
+			It("returns false for mine.", func() {
+				isDatabaseMine := adminConnection.DatabaseMine(gormDB, database)
+				Expect(isDatabaseMine).To(BeFalse())
+			})
+		})
+
+		Context("Database does exist in the table, and in the server, but not my database name", func() {
+			BeforeEach(func() {
+				managedDatabase.DatabaseName = "wrongdatabasename"
+			})
+			It("returns false for mine.", func() {
+				isDatabaseMine := adminConnection.DatabaseMine(gormDB, database)
+				Expect(isDatabaseMine).To(BeFalse())
+			})
+		})
+	})
 })
