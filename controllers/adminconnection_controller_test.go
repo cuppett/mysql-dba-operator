76487bf295df09f7e2f6d2b2a3d210ac2970b026
Stephen Cuppett <scuppett@redhat.com>
2023-06-18 18:01:40 -0400

    Fix: nil loop.adminConnection causing reconcile errors
    Also, added tests for the happy paths in Database and DatabaseUser

--- controllers/adminconnection_controller_test.go
+++ controllers/adminconnection_controller_test.go
@@ -3,14 +3,13 @@ package controllers
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"time"
 
 	. "github.com/cuppett/mysql-dba-operator/api/v1alpha1"
 )
 
-var _ = Describe("Admin Connection", func() {
+var _ = Describe("AdminConnection", func() {
 
 	Describe("Testing AdminConnection for database and user happy paths", func() {
 
@@ -22,13 +21,7 @@ var _ = Describe("Admin Connection", func() {
 					Name:      adminConnection.Name,
 				}
 				err := k8sClient.Get(ctx, adminConnectionNamespacedName, serverAdminConnection)
-				if err != nil {
-					if errors.IsNotFound(err) {
-						return "AdminConnection resource not found. Object must be deleted"
-					}
-					// Error reading the object - requeue the request.
-					return err.Error()
-				}
+				Expect(err).ToNot(HaveOccurred())
 				return serverAdminConnection.Status.Message
 			}).WithContext(ctx).Should(Equal("Successfully pinged database"))
 		}, NodeTimeout(time.Second*30))
